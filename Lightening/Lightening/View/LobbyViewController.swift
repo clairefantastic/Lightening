25ebf3f55cdcc3e3deb9cacea9a72b85396495c2
Claire Chiou <clairefantastic7@gmail.com>
2022-04-11 11:48:46 +0800

    Set LobbyViewController as RootViewController in SceneDelegate

---
+++ Lightening/Lightening/View/LobbyViewController.swift
@@ -0,0 +1,60 @@
+//
+//  LobbyViewController.swift
+//  Lightening
+//
+//  Created by claire on 2022/4/11.
+//
+
+import UIKit
+import AVFoundation
+import WebRTC
+
+//class LobbyViewController: UIViewController {
+//
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//
+//        // Do any additional setup after loading the view.
+//    }
+//
+//
+//    /*
+//    // MARK: - Navigation
+//
+//    // In a storyboard-based application, you will often want to do a little preparation before navigation
+//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//        // Get the new view controller using segue.destination.
+//        // Pass the selected object to the new view controller.
+//    }
+//    */
+//
+//}
+
+
+class LobbyViewController: UIViewController {
+    
+//    private let signalClient: SignalingClient
+//    private let webRTCClient: WebRTCClient
+    
+    private var currentPerson = ""
+    private var oppositePerson = ""
+    
+
+//    init(signalClient: SignalingClient, webRTCClient: WebRTCClient) {
+//      self.signalClient = signalClient
+//      self.webRTCClient = webRTCClient
+//      super.init(nibName: String(describing: LobbyViewController.self), bundle: Bundle.main)
+//    }
+//    
+//    @available(*, unavailable)
+//    required init?(coder aDecoder: NSCoder) {
+//      fatalError("init(coder:) has not been implemented")
+//    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
+
+
