6c364503c2e9be972df3b5f94b97cc6d60265c6a
Claire Chiou <clairefantastic7@gmail.com>
2022-04-11 11:58:34 +0800

    Initialize SignalClient and WebRTCClient in LobbyViewController

--- Lightening/Lightening/View/LobbyViewController.swift
+++ Lightening/Lightening/View/LobbyViewController.swift
@@ -9,50 +9,29 @@ import UIKit
 import AVFoundation
 import WebRTC
 
-//class LobbyViewController: UIViewController {
-//
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//
-//        // Do any additional setup after loading the view.
-//    }
-//
-//
-//    /*
-//    // MARK: - Navigation
-//
-//    // In a storyboard-based application, you will often want to do a little preparation before navigation
-//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-//        // Get the new view controller using segue.destination.
-//        // Pass the selected object to the new view controller.
-//    }
-//    */
-//
-//}
-
-
 class LobbyViewController: UIViewController {
     
-//    private let signalClient: SignalingClient
-//    private let webRTCClient: WebRTCClient
+    private let signalClient: SignalingClient
+    private let webRTCClient: WebRTCClient
     
     private var currentPerson = ""
     private var oppositePerson = ""
     
 
-//    init(signalClient: SignalingClient, webRTCClient: WebRTCClient) {
-//      self.signalClient = signalClient
-//      self.webRTCClient = webRTCClient
-//      super.init(nibName: String(describing: LobbyViewController.self), bundle: Bundle.main)
-//    }
-//    
-//    @available(*, unavailable)
-//    required init?(coder aDecoder: NSCoder) {
-//      fatalError("init(coder:) has not been implemented")
-//    }
+    init(signalClient: SignalingClient, webRTCClient: WebRTCClient) {
+      self.signalClient = signalClient
+      self.webRTCClient = webRTCClient
+      super.init(nibName: String(describing: LobbyViewController.self), bundle: Bundle.main)
+    }
+    
+    @available(*, unavailable)
+    required init?(coder aDecoder: NSCoder) {
+      fatalError("init(coder:) has not been implemented")
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("go")
 
     }
 }
