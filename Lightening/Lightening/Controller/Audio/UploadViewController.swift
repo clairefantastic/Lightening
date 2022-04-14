5332725fb27a79481b60c3bd2e10a7fdef149f9f
Claire Chiou <clairefantastic7@gmail.com>
2022-04-14 17:43:57 +0800

    Add SelectFile button

--- Lightening/Lightening/Controller/Audio/UploadViewController.swift
+++ Lightening/Lightening/Controller/Audio/UploadViewController.swift
@@ -9,8 +9,37 @@ import UIKit
 
 class UploadViewController: UIViewController {
     
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.view.backgroundColor = .white
+        
+        layoutButton()
+        
+    }
+    
+    func layoutButton() {
+        let selectFileButton = UIButton()
+        
+        self.view.addSubview(selectFileButton)
+        
+        selectFileButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint(item: selectFileButton, attribute: .top, relatedBy: .equal, toItem: view.safeAreaLayoutGuide, attribute: .top, multiplier: 1, constant: 100).isActive = true
+        
+        NSLayoutConstraint(item: selectFileButton, attribute: .width, relatedBy: .equal, toItem: view, attribute: .width, multiplier: 2/3, constant: 0).isActive = true
+        
+        NSLayoutConstraint(item: selectFileButton, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: 40).isActive = true
+        
+        NSLayoutConstraint(item: selectFileButton, attribute: .centerX, relatedBy: .equal, toItem: view, attribute: .centerX, multiplier: 1, constant: 0).isActive = true
+        
+        selectFileButton.backgroundColor = .black
+        
+        selectFileButton.setTitle("Select File", for: .normal)
+        
+        selectFileButton.isEnabled = true
     }
 }
 
