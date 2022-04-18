653a423462939f97bd3c8c98f5ee41ba5fb391a8
Claire Chiou <clairefantastic7@gmail.com>
2022-04-18 17:32:09 +0800

    Adjust Model related to Audio

---
+++ Lightening/Lightening/Model/Audio/AudioTopic.swift
@@ -0,0 +1,19 @@
+//
+//  AudioTopic.swift
+//  Lightening
+//
+//  Created by claire on 2022/4/18.
+//
+
+import Foundation
+
+enum AudioTopic {
+    
+    case nature
+    
+    case city
+    
+    case animal
+    
+    case others
+}
