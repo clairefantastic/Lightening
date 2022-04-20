fd23e2f0985b245a057e836b0411055c33b06665
Claire Chiou <clairefantastic7@gmail.com>
2022-04-20 17:15:28 +0800

    Create MapViewController

---
+++ Lightening/Lightening/Controller/Discovery/MapViewController.swift
@@ -0,0 +1,15 @@
+//
+//  MapViewController.swift
+//  Lightening
+//
+//  Created by claire on 2022/4/20.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
