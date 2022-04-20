1cf6b51178462225799d2bace3495b0ce1076933
Claire Chiou <clairefantastic7@gmail.com>
2022-04-20 13:37:29 +0800

    Pin user on map with current location

--- Lightening/Lightening/View/Upload/AddDetailsLocationCell.swift
+++ Lightening/Lightening/View/Upload/AddDetailsLocationCell.swift
@@ -7,31 +7,85 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
-class AddDetailsLocationCell: UITableViewCell, MKMapViewDelegate {
+class AddDetailsLocationCell: UITableViewCell, MKMapViewDelegate, CLLocationManagerDelegate {
 
     @IBOutlet weak var mapView: MKMapView!
     
     @IBOutlet weak var categoryLabel: UILabel!
     
-    let locationManager = CLLocationManager()
+    private let locationManager = CLLocationManager()
+    
+    private var currentPlace: CLPlacemark?
+    
+    var currentLocationStr = "Current location"
     
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
-        locationManager.requestWhenInUseAuthorization()
+        determineCurrentLocation()
         
         mapView.delegate = self
-        
-        let location = CLLocation(latitude: 22.999696, longitude: 120.212768)
-        let region = MKCoordinateRegion(center: location.coordinate, latitudinalMeters: 300, longitudinalMeters: 300)
-        mapView.setRegion(region, animated: true)
-    }
 
+    }
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
         // Configure the view for the selected state
     }
     
+    private func determineCurrentLocation() {
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestAlwaysAuthorization()
+
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.startUpdatingLocation()
+        }
+    }
+
 }
+
+extension AddDetailsLocationCell {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let mUserLocation:CLLocation = locations[0] as CLLocation
+
+        let center = CLLocationCoordinate2D(latitude: mUserLocation.coordinate.latitude, longitude: mUserLocation.coordinate.longitude)
+        let mRegion = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
+
+        mapView.setRegion(mRegion, animated: true)
+        
+        let mkAnnotation: MKPointAnnotation = MKPointAnnotation()
+            mkAnnotation.coordinate = CLLocationCoordinate2DMake(mUserLocation.coordinate.latitude, mUserLocation.coordinate.longitude)
+            mkAnnotation.title = self.setUsersClosestLocation(mLattitude: mUserLocation.coordinate.latitude, mLongitude: mUserLocation.coordinate.longitude)
+            mapView.addAnnotation(mkAnnotation)
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("Error - locationManager: \(error.localizedDescription)")
+    }
+    
+    func setUsersClosestLocation(mLattitude: CLLocationDegrees, mLongitude: CLLocationDegrees) -> String {
+        let geoCoder = CLGeocoder()
+        let location = CLLocation(latitude: mLattitude, longitude: mLongitude)
+
+        geoCoder.reverseGeocodeLocation(location) {
+            (placemarks, error) -> Void in
+
+            if let mPlacemark = placemarks{
+                if let dict = mPlacemark[0].addressDictionary as? [String: Any]{
+                    if let Name = dict["Name"] as? String{
+                        if let City = dict["City"] as? String{
+                            self.currentLocationStr = Name + ", " + City
+                        }
+                    }
+                }
+            }
+        }
+        return currentLocationStr
+    }
+
+}
+
