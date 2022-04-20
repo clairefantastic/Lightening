c1ccd010a64ecb9d0c08ccfd7e9a109c18b6d56d
Claire Chiou <clairefantastic7@gmail.com>
2022-04-20 17:46:18 +0800

    Add Map View Constraint

--- Lightening/Lightening/Controller/Discovery/MapViewController.swift
+++ Lightening/Lightening/Controller/Discovery/MapViewController.swift
@@ -6,10 +6,35 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class MapViewController: UIViewController {
+class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
+    
+    private let mapView = MKMapView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        layoutMapView()
+    }
+}
+
+extension MapViewController {
+    
+    func layoutMapView() {
+        
+        view.addSubview(mapView)
+        
+        mapView.translatesAutoresizingMaskIntoConstraints = false
+        
+        mapView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        
+        mapView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        
+        mapView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        
+        mapView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        
     }
 }
