feba9de8da7640b07611937e08c327d3936b5ca1
Claire Chiou <clairefantastic7@gmail.com>
2022-04-21 20:26:31 +0800

    Create AudioDescriptionViewController

---
+++ Lightening/Lightening/Controller/Discovery/AudioDescriptionViewController.swift
@@ -0,0 +1,15 @@
+//
+//  AudioDescriptionViewController.swift
+//  Lightening
+//
+//  Created by claire on 2022/4/21.
+//
+
+import UIKit
+
+class AudioDescriptionViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
