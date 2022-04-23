f678e88532b0b5b5e7525c6e6464ba0b13ce7f05
Claire Chiou <clairefantastic7@gmail.com>
2022-04-23 17:15:19 +0800

    Create circular text in UserProfileView

--- Lightening/Lightening/Controller/User/ProfileViewController.swift
+++ Lightening/Lightening/Controller/User/ProfileViewController.swift
@@ -9,8 +9,21 @@ import UIKit
 
 class ProfileViewController: BaseViewController {
     
+    let userProfileView = UserProfileView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        addUserProfileView()
         
+    }
+}
+
+extension ProfileViewController {
+    
+    private func addUserProfileView() {
+        
+        self.view.stickSubView(userProfileView, inset: UIEdgeInsets(top: 80, left: width - 160, bottom: height - 240, right: 24))
+        
+        userProfileView.backgroundColor =  UIColor.hexStringToUIColor(hex: "#D65831")
     }
 }
