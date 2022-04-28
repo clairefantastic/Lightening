17caaa18668dcb7faab7982d0d3cf9f5394b6338
Claire Chiou <clairefantastic7@gmail.com>
2022-04-28 09:04:52 +0800

    Create AudioListViewController and AudioListTableViewCell

---
+++ Lightening/Lightening/Controller/Discovery/AudioListViewController.swift
@@ -0,0 +1,8 @@
+//
+//  AudioListViewController.swift
+//  Lightening
+//
+//  Created by claire on 2022/4/28.
+//
+
+import Foundation
---
+++ Lightening/Lightening/View/Discovery/AudioListTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  AudioListTableViewCell.swift
+//  Lightening
+//
+//  Created by claire on 2022/4/28.
+//
+
+import UIKit
+
+class AudioListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
