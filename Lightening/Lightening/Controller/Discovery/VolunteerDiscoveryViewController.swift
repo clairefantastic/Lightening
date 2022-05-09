1ccf190d425124bd6c870a6edf447ed9521b9402
Claire Chiou <clairefantastic7@gmail.com>
2022-05-09 14:27:31 +0800

    Add daily picks function

--- Lightening/Lightening/Controller/Discovery/VolunteerDiscoveryViewController.swift
+++ Lightening/Lightening/Controller/Discovery/VolunteerDiscoveryViewController.swift
@@ -70,6 +70,31 @@ extension VolunteerDiscoveryViewController {
         
         self.navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "magnifyingglass"), style: UIBarButtonItem.Style.plain, target: self, action: #selector(pushSearchPage))
         self.navigationItem.rightBarButtonItem?.tintColor = .black
+        
+        self.navigationItem.leftBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "sparkles"), style: UIBarButtonItem.Style.plain, target: self, action: #selector(dailyPicks))
+        self.navigationItem.leftBarButtonItem?.tintColor = .black
+    }
+    
+    @objc func dailyPicks(_ sender: UIBarButtonItem) {
+        
+        let tabBarHeight = self.tabBarController?.tabBar.intrinsicContentSize.height ?? 50
+        
+        let audioPlayerViewController = AudioPlayerViewController()
+        addChild(audioPlayerViewController)
+        audioPlayerViewController.audio = self.audios[Int.random(in: 0..<self.audios.count)]
+        audioPlayerViewController.view.backgroundColor?.withAlphaComponent(0)
+        view.addSubview(audioPlayerViewController.view)
+        audioPlayerViewController.view.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint(item: audioPlayerViewController.view, attribute: .centerX, relatedBy: .equal,
+                           toItem: self.view.safeAreaLayoutGuide, attribute: .centerX, multiplier: 1, constant: 0).isActive = true
+        
+        NSLayoutConstraint(item: audioPlayerViewController.view, attribute: .bottom, relatedBy: .equal,
+                           toItem: self.view.safeAreaLayoutGuide, attribute: .bottom, multiplier: 1, constant: 0).isActive = true
+        
+        NSLayoutConstraint(item: audioPlayerViewController.view, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: 80).isActive = true
+        
+        NSLayoutConstraint(item: audioPlayerViewController.view, attribute: .width, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: width).isActive = true
     }
     
     @objc func pushSearchPage(_ sender: UIBarButtonItem) {
