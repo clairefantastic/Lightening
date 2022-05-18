e56d9e6c27c78965c9d00b3be085c7c12f669049
Claire Chiou <clairefantastic7@gmail.com>
2022-05-18 10:28:44 +0800

    Delete navigationBarView

--- Lightening/Lightening/Controller/Base/BaseViewController.swift
+++ Lightening/Lightening/Controller/Base/BaseViewController.swift
@@ -8,8 +8,6 @@
 import UIKit
 
 class BaseViewController: UIViewController {
-    
-    let navigationBarView = NavigationBarView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,17 +19,6 @@ class BaseViewController: UIViewController {
         navigationController?.navigationBar.tintColor = UIColor.black
         
         navigationController?.navigationBar.titleTextAttributes = [.font: UIFont(name: "American Typewriter Bold", size: 20)]
-        
-        let view = UIView(frame: CGRect(x: 0, y: 0, width: 200, height: 100))
-        let imageView = UIImageView()
-        imageView.image = UIImage(named: "Map")
-        imageView.contentMode = .scaleAspectFit
-        view.addSubview(imageView)
-        imageView.translatesAutoresizingMaskIntoConstraints = false
-
-        navigationBarView.layoutImageView()
-        
-        navigationController?.navigationItem.titleView?.addSubview(navigationBarView)
         
     }
 
