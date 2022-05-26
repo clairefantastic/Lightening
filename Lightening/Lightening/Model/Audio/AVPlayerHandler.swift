90c5e8627f82fc0eda796d021da99bd6124fe87c
Claire Chiou <clairefantastic7@gmail.com>
2022-05-26 14:36:18 +0800

    Add AudioLength Test

--- Lightening/Lightening/Model/Audio/AVPlayerHandler.swift
+++ Lightening/Lightening/Model/Audio/AVPlayerHandler.swift
@@ -13,6 +13,12 @@ class AVPlayerHandler {
     
     var player: AVPlayer!
     
+//    let url = URL(string: urlString)!
+    
+    let urlString = "https://firebasestorage.googleapis.com/v0/b/lightening-626ce.appspot.com/o/message_voice%2FB51ED7F6-F5BF-4B21-9B50-7610D86BF5B1.m4a?alt=media&token=9a78cdac-eda3-46a4-8d50-8a68fb8c9971"
+    
+    var duration = 0
+    
     func setPlayer(url: URL) {
         
         let asset = AVAsset(url: url)
@@ -35,5 +41,23 @@ class AVPlayerHandler {
         let duration = playerItem.asset.duration
         
         return CMTimeGetSeconds(duration)
+    }
+    
+    func setPlayer(urlString: String) -> AVPlayerItem {
+        
+        let url = URL(string: urlString)!
+        
+        let asset = AVAsset(url: url)
+        
+        let playerItem = AVPlayerItem(asset: asset)
+        
+        return playerItem
+    }
+    
+    func getAudioLengthTest(avPlayerItem: AVPlayerItem) -> Int {
+        
+        duration = Int(CMTimeGetSeconds(avPlayerItem.asset.duration))
+        
+        return duration
     }
 }
--- Lightening/LighteningTests/LighteningTests.swift
+++ Lightening/LighteningTests/LighteningTests.swift
@@ -6,16 +6,34 @@
 //
 
 import XCTest
+@testable import Lightening
 
 class LighteningTests: XCTestCase {
+    var sut: AVPlayerHandler!
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        try super.setUpWithError()
+        sut = AVPlayerHandler()
     }
 
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
+        sut = nil
+        try super.tearDownWithError()
     }
+    
+    func testGetAudioLength() {
+        // 1. given
+        let avPlayerItem = sut.setPlayer(urlString: "https://firebasestorage.googleapis.com/v0/b/lightening-626ce.appspot.com/o/message_voice%2FB51ED7F6-F5BF-4B21-9B50-7610D86BF5B1.m4a?alt=media&token=9a78cdac-eda3-46a4-8d50-8a68fb8c9971")
+        
+        // 2. when
+        sut.getAudioLengthTest(avPlayerItem: avPlayerItem)
+        
+        // 3. then
+        XCTAssertEqual(sut.duration, 12, "Audio length is wrong")
+    }
+
 
     func testExample() throws {
         // This is an example of a functional test case.
