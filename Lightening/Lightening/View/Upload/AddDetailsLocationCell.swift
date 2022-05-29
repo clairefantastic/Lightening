427e51111010fd87da34e13580ccb3587be4af5d
Claire Chiou <clairefantastic7@gmail.com>
2022-05-30 01:33:56 +0800

    Delete unused code in AddDetailsLocationCell

--- Lightening/Lightening/View/Upload/AddDetailsLocationCell.swift
+++ Lightening/Lightening/View/Upload/AddDetailsLocationCell.swift
@@ -25,17 +25,18 @@ class AddDetailsLocationCell: AddDetailsBasicCell, MKMapViewDelegate, CLLocation
 
         // Initialization code
         self.backgroundColor = UIColor.lightBlue
-        
         mapView.delegate = self
-        
         mapView.layer.cornerRadius = 10
-
     }
     
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
         // Configure the view for the selected state
+    }
+    
+    override func layoutCell(title: String) {
+        categoryLabel.text = title
     }
     
     func determineCurrentLocation() {
@@ -52,8 +53,8 @@ class AddDetailsLocationCell: AddDetailsBasicCell, MKMapViewDelegate, CLLocation
 
 extension AddDetailsLocationCell {
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
         let mUserLocation:CLLocation = locations[0] as CLLocation
-
         let center = CLLocationCoordinate2D(latitude: mUserLocation.coordinate.latitude, longitude: mUserLocation.coordinate.longitude)
         let mRegion = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
 
@@ -61,8 +62,8 @@ extension AddDetailsLocationCell {
         
         let mkAnnotation: MKPointAnnotation = MKPointAnnotation()
         mkAnnotation.coordinate = CLLocationCoordinate2DMake(mUserLocation.coordinate.latitude, mUserLocation.coordinate.longitude)
-        mkAnnotation.title = self.setUsersClosestLocation(mLattitude: mUserLocation.coordinate.latitude, mLongitude: mUserLocation.coordinate.longitude)
-            
+//        mkAnnotation.title = self.setUsersClosestLocation(mLattitude: mUserLocation.coordinate.latitude, mLongitude: mUserLocation.coordinate.longitude)
+//
         mapView.addAnnotation(mkAnnotation)
         
         locationHandler?(Location(latitude: mUserLocation.coordinate.latitude, longitude: mUserLocation.coordinate.longitude))
@@ -71,25 +72,6 @@ extension AddDetailsLocationCell {
     
     func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
         print("Error - locationManager: \(error.localizedDescription)")
-    }
-    
-    func setUsersClosestLocation(mLattitude: CLLocationDegrees, mLongitude: CLLocationDegrees) -> String {
-        let geoCoder = CLGeocoder()
-        let location = CLLocation(latitude: mLattitude, longitude: mLongitude)
-
-        geoCoder.reverseGeocodeLocation(location) {
-            (placemarks, error) -> Void in
-            if let mPlacemark = placemarks{
-                if let dict = mPlacemark[0].addressDictionary as? [String: Any] {
-//                    if let Name = dict["Name"] as? String{
-//                        if let City = dict["City"] as? String{
-//                            self.currentLocationStr = Name + ", " + City
-//                        }
-//                    }
-                }
-            }
-        }
-        return currentLocationStr
     }
 
 }
